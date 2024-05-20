c7aeae00ade389d85045ee494208865e8344d4c7
AnastasiiaDia <anastasiia.iv@gmail.com>
2024-05-20 15:49:54 +0200

    added background-image and after-effects hover-effects to footer, styles to header

---
+++ js/modal.js
@@ -0,0 +1,9 @@
+const modal = document.querySelector(".backdrop");
+
+const modalIsOpen = document.querySelector(".modal-open");
+const modalIsClose = document.querySelector(".modal-close");
+
+const toggleModal = () => modal.classList.toggle("is-hidden");
+
+modalIsOpen.addEventListener("click", toggleModal);
+modalIsClose.addEventListener("click", toggleModal);
