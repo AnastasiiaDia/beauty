2b23602f75625728620a596e9382ef1853d09702
AnastasiiaDia <anastasiia.iv@gmail.com>
2024-05-23 23:54:27 +0200

    added form

--- js/modal.js
+++ js/modal.js
@@ -1,9 +1,9 @@
-const modal = document.querySelector(".backdrop");
+const modal = document.querySelector('.backdrop');
 
-const modalIsOpen = document.querySelector(".modal-open");
-const modalIsClose = document.querySelector(".modal-close");
+const modalIsOpen = document.querySelector('.modal-open');
+const modalIsClose = document.querySelector('.modal-close');
 
-const toggleModal = () => modal.classList.toggle("is-hidden");
+const toggleModal = () => modal.classList.toggle('is-hidden');
 
-modalIsOpen.addEventListener("click", toggleModal);
-modalIsClose.addEventListener("click", toggleModal);
+modalIsOpen.addEventListener('click', toggleModal);
+modalIsClose.addEventListener('click', toggleModal);
